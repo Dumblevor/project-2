4c0c7711c9ce1554769f53c6ab02277618cba9f4
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-05-21 13:54:57 +0300

    css changes and improvements

--- src/components/About.js
+++ src/components/About.js
@@ -2,24 +2,24 @@ import React from "react"
 
 function About() {
   return (
-    <div className="container-about card">
-      <section className="card">
+    <div className="container-about card ">
+      <section className="card aboutcard">
         <h2>Creators</h2>
         <h3>This project was developed by Dimitar and Laleh as part of General Assembley Softwear engineer bootcamp.</h3>
-        <h3>Instructions by: <strong>Laleh</strong></h3>
-        <h3>Logo by: <strong>Dimitar</strong></h3>
+        <p>Instructions by: <strong>Laleh</strong></p>
+        <p>Logo by: <strong>Dimitar</strong></p>
       </section>
 
-      <section className="card">
+      <section className="card aboutcard">
         <h2>Other Assets</h2>
-        <h3>API utilized: <a href="https://rickandmortyapi.com">https://rickandmortyapi.com</a></h3>
-        <h3>CSS packaged used: <a href="https://bulma.io">https://bulma.io</a></h3>
+        <p>API utilized: <a href="https://rickandmortyapi.com">https://rickandmortyapi.com</a></p>
+        <p>CSS packaged used: <a href="https://bulma.io">https://bulma.io</a></p>
       </section>
 
-      <section className="card">
+      <section className="card aboutcard">
         <h2>Check out our other projects:</h2>
-        <h4>Dimitar&rsquo; s Space Invaders (project 1) <a href="https://dumblevor.github.io/spce_inv1/">https://dumblevor.github.io/spce_inv1/</a></h4>
-        <h4>Laleh&rsquo; s Pac-Man (project 1) <a href="https://laleh-s.github.io/GA-Project-1/">https://laleh-s.github.io/GA-Project-1/</a></h4>
+        <p>Dimitar&rsquo; s Space Invaders (Project #1): <a href="https://dumblevor.github.io/spce_inv1/">https://dumblevor.github.io/spce_inv1/</a></p>
+        <p>Laleh&rsquo; s Pac-Man (Project #1): <a href="https://laleh-s.github.io/GA-Project-1/">https://laleh-s.github.io/GA-Project-1/</a></p>
       </section>
     </div>
 
